6cc41705823b1e9954ed36de96a88933cf38f42b
Stephan <stephanfknvisser@gmail.com>
2022-06-11 04:19:57 +0200

    main.py setup and calculator.py addition

---
+++ calculator.py
@@ -0,0 +1,5 @@
+# takes in the amount of wire used and batteries used, and calculates the cost
+
+
+def calculate_cost(wires, batteries) -> float:
+    pass
--- main.py
+++ main.py
@@ -1,11 +1,49 @@
-# alles init 
+# alles init
 # make wires runnen
 # kosten berekenen en opslaan
 
-from houses import Houses, House
+import argparse
+from houses import Houses
 from batteries import Batteries
 from wires import Wires
+from calculator import calculate_cost
+
+
+def main(houses_input, batteries_input, output):
+
+    # init
+    houses = Houses()
+    batteries = Batteries()
+    wires = Wires()
+
+    # loading
+    houses.load(houses_input)
+    batteries.load(batteries_input)
+
+    # generate wires
+    # TODO
+
+    # plot grid and save
+    # TODO
+
+    return calculate_cost(wires, batteries)
 
 
 if __name__ == "__main__":
-    Houses = Houses()
+    # Set-up parsing command line arguments
+    parser = argparse.ArgumentParser(description=
+                "Generates a SmartGrid for input houses and batteries")
+
+    # Adding arguments
+    parser.add_argument("houses_input",
+        help="input file for the houses (csv)", required=True)
+    parser.add_argument("batteries_input",
+        help="input file for the batteries (csv)", required=True)
+    parser.add_argument("output",
+        help="output file for the plotted grid (png)")
+
+    # Read arguments from command line
+    args = parser.parse_args()
+
+    # Run our line function with provided arguments
+    main(args.houses_input, args.batteries_input, args.output)
