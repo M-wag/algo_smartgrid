b4e95259b67cde4153c0912225ca640eb4568ba8
Thomas Been <thomasbeen@live.nl>
2022-06-13 13:27:58 +0200

    main batch run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import argparse
-
+import json
 from houses import Houses
 from batteries import Batteries
 from wires import Wires
@@ -7,22 +7,24 @@ from calculator import calculate_cost
 from visualize import visualize_grid
 
 
-def main(houses_input: str, batteries_input: str, output: str) -> float:
-
-    # init
-    houses = Houses(houses_input)
-    batteries = Batteries(batteries_input)
-    wires = Wires()
-
+def main(houses_input: str, batteries_input: str, output_pictures: str, output_file: str, n: int) -> None:
+    lowest_cost = 0 
     # Generate wires
-    wires.generate(houses, batteries)
-
-    # Plot grid and save
-    visualize_grid(houses.get_member_coords(),
+    for i in range(n):
+        # init
+        houses = Houses(houses_input)
+        batteries = Batteries(batteries_input)
+        wires = Wires()
+        # Make wires 
+        if wires.generate(houses, batteries):
+            cost = calculate_cost(houses, batteries)
+            if cost < lowest_cost:
+                lowest_cost = cost
+                # Plot grid and save
+                visualize_grid(houses.get_member_coords(),
                    batteries.get_member_coords(),
                    wires.get_paths())
 
-    return calculate_cost(houses, batteries)
 
 
 if __name__ == "__main__":
@@ -35,12 +37,18 @@ if __name__ == "__main__":
                         help="input file for the houses (csv)")
     parser.add_argument("batteries_input",
                         help="input file for the batteries (csv)")
-    parser.add_argument("--output",
+    parser.add_argument("--output_picture",
                         help="output file for the plotted grid (png)",
+                        required=False)
+    parser.add_argument("--output_file",
+                        help="output file for lowest cost (json)",
+                        required=False)
+    parser.add_argument("--n",
+                        help="Number of iterations",
                         required=False)
 
     # Read arguments from command line
     args = parser.parse_args()
 
     # Run our line function with provided arguments
-    main(args.houses_input, args.batteries_input, args.output)
+    main(args.houses_input, args.batteries_input, args.output_pictures, args.output_file, args.n)
