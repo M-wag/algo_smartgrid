a9a9883544eeea5868b9fb3871d7d50e404f4447
Stephan <stephanfknvisser@gmail.com>
2022-06-22 23:57:54 +0200

    2 new pathfinder functions, further code cleanup

--- path_finders.py
+++ path_finders.py
@@ -1,7 +1,6 @@
 from typing import Tuple, List, Set
 import random
 import math
-import time
 
 def calc_distance(L1, L2, P):
     x_start, y_start = L1
@@ -13,8 +12,25 @@ def calc_distance(L1, L2, P):
     
     return distance
 
+def pathfinder_directions(x_path, y_path, x_end, y_end):
+    # Get difference between X and Y
+    x_dif = x_end - x_path
+    y_dif = y_end - y_path
 
-def random_path_finder(starting_coord: Tuple[int, int],
+    # pos dif = 1, neg dif = -1, no dif = 0:
+    if x_dif == 0:
+        x_dir = 0
+    else:
+        x_dir = int(x_dif / abs(x_dif))
+
+    if y_dif == 0:
+        y_dir = 0
+    else:
+        y_dir = int(y_dif / abs(y_dif))
+
+    return x_dir, y_dir
+
+def random_pathfinder(starting_coord: Tuple[int, int],
                        end_coord: Tuple[int, int]) -> List[Tuple[int, int]]:
     '''
     Generates a path between start- and end-coordinates,
@@ -39,34 +55,74 @@ def random_path_finder(starting_coord: Tuple[int, int],
     x_path, y_path = starting_coord
     x_end, y_end = end_coord
 
-    # Get difference between X and Y
-    x_dif = x_end - x_path
-    y_dif = y_end - y_path
+    # get the x- and y-directions (1, -1 or 0 equal to the step size)
+    x_dir, y_dir = pathfinder_directions(x_path, y_path, x_end, y_end)
 
-    # pos dif = 1, neg dif = -1, no dif = 0:
-    if x_dif == 0:
-        next_move_x = 0
-    else:
-        next_move_x = int(x_dif / abs(x_dif))
+    # add all the necessary moves into a list,
+    # number of moves is equal to the horizontal and vertical difference
+    moves = []
+    for x_move in range(abs(x_end - x_path)):
+        moves.append(("x_move", x_dir))
 
-    if y_dif == 0:
-        next_move_y = 0
-    else:
-        next_move_y = int(y_dif / abs(y_dif))
-
-    # moves = []
-    # for x in range(abs(x_dif)):
-    #     moves.append(("x_path", next_move_x))
-    # for y in range(abs(y_dif)):
-    #     moves.append(("y_path", next_move_y))
+    for y_move in range(abs(y_end - y_path)):
+        moves.append(("y_move", y_dir))
     
-    # random.shuffle(moves)
+    # shuffle the order of the moves
+    random.shuffle(moves)
+
+    # add the starting point to the list and iterate over the moves
+    path = [(x_path, y_path)]
+    for move in moves:
+        if move[0] == "x_move":
+            x_path += move[1]
+        else:
+            y_path += move[1]
+        
+        # add all coordinates to the path-list
+        path.append((x_path, y_path))
+
+    return path
+
+
+def hor_vert_pathfinder(starting_coord: Tuple[int, int],
+                       end_coord: Tuple[int, int]) -> List[Tuple[int, int]]:
+
+    # Get X and Y for starting- and end-coord
+    x_path, y_path = starting_coord
+    x_end, y_end = end_coord
+
+    # get the x- and y-directions (1, -1 or 0 equal to the step size)
+    x_dir, y_dir = pathfinder_directions(x_path, y_path, x_end, y_end)
+
+    # add the starting coordinate to the path-list
+    path = [(x_path, y_path)]
+
+    # Draw the horizontal line
+    while x_path != x_end:
+        x_path += x_dir
+        path.append((x_path, y_path))
+    
+    # Draw the vertical line
+    while y_path != y_end:
+        y_path += y_dir
+        path.append((x_path, y_path))
+
+    return path
+
+def straight_pathfinder(starting_coord: Tuple[int, int],
+                       end_coord: Tuple[int, int]) -> List[Tuple[int, int]]:
+    # Get X and Y for starting- and end-coord
+    x_path, y_path = starting_coord
+    x_end, y_end = end_coord
+
+    # get the x- and y-directions (1, -1 or 0 equal to the step size)
+    x_dir, y_dir = pathfinder_directions(x_path, y_path, x_end, y_end)
 
     path = [(x_path, y_path)]
     
     while (x_path, y_path) != (x_end, y_end):
-        x_test = (x_path + next_move_x, y_path)
-        y_test = (x_path, y_path + next_move_y)
+        x_test = (x_path + x_dir, y_path)
+        y_test = (x_path, y_path + y_dir)
 
         # calculate distance from the two point to the house-battery line
         x_move_dis = calc_distance(starting_coord, end_coord, x_test)
@@ -74,33 +130,15 @@ def random_path_finder(starting_coord: Tuple[int, int],
 
         # choose the option closest to the line, favoring the x_direction
         if x_path == x_end:
-            y_path += next_move_y
-            path.append((x_path, y_path))
+            y_path += y_dir
         elif y_path == y_end:
-            x_path += next_move_x
-            path.append((x_path, y_path))
+            x_path += x_dir
         elif x_move_dis <= y_move_dis:
-            x_path += next_move_x
+            x_path += x_dir
         else:
-            y_path += next_move_y
-        path.append((x_path, y_path))
-
-    # Draw the X line
-    # while x_path != x_end:
-    #     x_path += next_move_x
-    #     path.append((x_path, y_path))
-    
-    # # Draw the Y line
-    # while y_path != y_end:
-    #     y_path += next_move_y
-    #     path.append((x_path, y_path))
-
-    # for move in moves:
-    #     if move[0] == "x_path":
-    #         x_path += move[1]
-    #     else:
-    #         y_path += move[1]
-    #     path.append((x_path, y_path))
+            y_path += y_dir
         
-
+        # add the resulting coordinate to the path-list
+        path.append((x_path, y_path))
+    
     return path
